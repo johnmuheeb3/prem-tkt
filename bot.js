2c06b5282f455aa189a4d623822e425629f56563
johnmuheeb3 <44290389+johnmuheeb3@users.noreply.github.com>
2019-03-20 18:07:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,31 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "-"
 
+lient.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`APEX`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 client.on('message', message => {
 if(message.author.bot) return;
 if(message.channel.type === 'dm') return;
