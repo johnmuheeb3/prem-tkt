19775ec70f229d02013f09b95ae7212573e431d5
johnmuheeb3 <44290389+johnmuheeb3@users.noreply.github.com>
2019-03-21 12:58:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = "-"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`APEX`,"http://twitch.tv/S-F")
+client.user.setGame(`-bc     | LèLèø ,❀`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
